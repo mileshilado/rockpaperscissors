1827613bedab30f39b6b2ed7ba3b684ab7012722
Miles Hilado <Mileshilado@yahoo.com>
2024-05-13 13:38:26 -0500

    Update rockpaper.js
    wrote logic for random computer choice

--- rockpaper.js
+++ rockpaper.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+    choice = Math.random();
+    if(choice < 0.3333){
+        return "rock"
+    }
+    else if(choice > 0.3333 && choice < 0.66666){
+        return "paper"
+    }
+    else if(choice > 0.66666){
+        return "scissors"
+    }
+}

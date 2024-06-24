5e5466bff8603b3665f1df66a040cd0427dc5693
Miles Hilado <Mileshilado@yahoo.com>
2024-06-23 19:36:01 -0500

    finished game

--- rockpaper.js
+++ rockpaper.js
@@ -1,5 +1,3 @@
-console.log(getHumanChoice());
-
 function getComputerChoice() {
     choice = Math.random();
     if(choice < 0.3333){
@@ -18,14 +16,60 @@ function getHumanChoice(){
     return choice
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    // your code here!
+function playRound() {
+    var humanchoice = getHumanChoice()
+    var computerchoice = getComputerChoice()
+    var winner
+    if(humanchoice === computerchoice){
+        winner = 'none'
+    }
+    else if(humanchoice == 'scissors' && computerchoice == 'rock'){;
+        winner = 'computer'
+    }
+    else if(humanchoice == 'rock' && computerchoice == 'paper'){
+        winner = 'computer'
+    }
+    else if(humanchoice == 'paper' && computerchoice == 'scissors'){
+        winner = 'computer'
+    }
+    else if(humanchoice == 'rock' && computerchoice == 'scissors'){
+        winner = 'human'
+    }
+    else if(humanchoice == 'paper' && computerchoice == 'rock'){
+        winner = 'human'
+    }
+    else if(humanchoice == 'scissors' && computerchoice == 'paper'){
+        winner = 'human'
+    }
+    else{
+        winner = 'none'
+    }
+    console.log(`${winner} wins the round`)
+    return winner
   }
-  
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  
-  playRound(humanSelection, computerSelection);
+
+function playGame(){
+    let humanScore = 0
+    let computerScore = 0
+
+    while(humanScore < 3 && computerScore < 3){
+        winner = playRound()
+        if(winner == "human"){
+            humanScore++
+            console.log(`Human score: ${humanScore}`);
+        }
+        else if(winner =="computer"){
+            computerScore++
+            console.log(`Computer score: ${computerScore}`);
+        }
+        else if(winner =="none"){
+            continue
+        }
+    }
+        if (humanScore === 3) {
+            console.log("You win the game!");
+        } else if (computerScore === 3) {
+            console.log("Computer wins the game!");
+        }
+    return
+}
